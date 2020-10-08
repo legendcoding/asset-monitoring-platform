28e16b852bfb884dc0e5d42a74267c572da28855
Yj-PC <yongjin91@gmail.com>
2020-10-08 17:54:04 +0800

    adding skeleton of frontend after clean up

---
+++ asset-monitoring-platform-frontend/ABOUT.md
@@ -0,0 +1,11 @@
+Generic Asset Monitoring Template Project:
+
+Hosting and Authentication: Firebase
+Front-end: React
+Back-end: MongoDB, Express.js, Node.js, Pusher
+
+Front-End
+[ ]
+
+Back-End
+[ ]
---
+++ asset-monitoring-platform-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world!!</h1>
+      <input/>
+      <button>Add Todo</button>
+    </div>
+  );
+}
+
+export default App;
